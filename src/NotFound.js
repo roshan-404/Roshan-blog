3b7cfbad70df40c4be83d035bc67bc66021b5947
Roshan Kumar Ojha <roshanboyrko@gmail.com>
2021-05-13 07:49:49 +0530

    Done with the Local App

---
+++ src/NotFound.js
@@ -0,0 +1,11 @@
+const NotFound = () => {
+    return ( 
+        <div classname='not-found'> 
+            <h2>Sorry!</h2>
+            <p>Page cannot be found</p>
+            <link to='/'>Back to the homepage...</link>
+        </div>
+     );
+}
+ 
+export default NotFound
